5cac66f16428ee4b1839510fad9827a58d829b95
Gurnot, Frederic <Frederic.Gurnot@nytimes.com>
2019-08-05 14:21:54 -0400

    remove unused files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import DockCadenceApp from './DockCadenceApp';
 import * as serviceWorker from './serviceWorker';
 
