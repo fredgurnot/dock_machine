2ae2d581a8a0eac8606272a22b864605fa0bfba9
Gurnot, Frederic <Frederic.Gurnot@nytimes.com>
2019-08-05 11:20:47 -0400

    initial commit

--- src/DockCadenceApp.js
+++ src/DockCadenceApp.js
@@ -3,52 +3,33 @@ import DockDisplay from './DockDisplay';
 import './DockCadenceApp.css';
 
 const DockCadenceApp = () => {
-    const [meterThreshold, setMeterThreshold] = useState(10);
-    const [meterArray, setMeterArray] = useState([]);
+
     const [cadenceString, setCadenceString] = useState("1");
     const [cadenceArray, setCadenceArray] = useState([]);
 
 
     useEffect(() => {
         setCadenceArray(cadenceString.split(',').map(Number));
+
     },[cadenceString]);
 
-    useEffect(() => {
-        const arr = [];
-        console.log('meterArray:' + meterArray);
-        for (let i = 1; i <= meterThreshold; i++) {
-            arr[i-1] = (cadenceArray.indexOf(meterThreshold-i) > -1) ? 1 : 0;
-        }
-        console.log('arr:' + arr);
-        setMeterArray(arr);
-    }, [meterThreshold]);
 
     return (
         <div>
-            <div className="meter-imput">
-                <label htmlFor="meterThreshold">
-                    Meter Threshold
-                    <input
-                        id="meterThreshold"
-                        value={meterThreshold}
-                        placeholder="Meter Threshold"
-                        onChange={ (e) => (setMeterThreshold(e.target.value))}
-                    />
-                </label>
-                <br/>
-                <label htmlFor="cadence">
-                    Audience: Remaing articles
-                    <input
-                        id="cadence"
-                        value={cadenceString}
-                        placeholder="Meter Cadence"
-                        onChange={ (e) => setCadenceString(e.target.value)}
-                    />
-                </label>
-            </div>
+            <label htmlFor="cadence">
+                Audience: Remaing articles
+                <input
+                    id="cadence"
+                    value={cadenceString}
+                    placeholder="Meter Cadence"
+                    onChange={ (e) => setCadenceString(e.target.value)}
+                />
+            </label>
+            <br/>
+
 
             <DockDisplay
-            meterArray={meterArray}
+                cadenceArray={cadenceArray}
             />
         </div>
     )
--- src/DockDisplay.js
+++ src/DockDisplay.js
@@ -1,22 +1,51 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Dock from './Dock';
-import  './DockDisplay.css'
+import './DockDisplay.css'
 
-const DockDisplay = ({meterArray}) => {
+const DockDisplay = ({cadenceArray}) => {
+
+    const [meterThreshold, setMeterThreshold] = useState(10);
+    const [meterArray, setMeterArray] = useState([]);
+
+    useEffect(() => {
+        const arr = [];
+        console.log('meterArray:' + meterArray);
+        for (let i = 1; i <= meterThreshold; i++) {
+            arr[i-1] = (cadenceArray.indexOf(meterThreshold-i) > -1) ? 1 : 0;
+        }
+        console.log('arr:' + arr);
+        setMeterArray(arr);
+    },[meterThreshold, cadenceArray]);
+
 
     return (
-        <div className="dock-display">
-            {
-                meterArray.map((dock,i) => {
-                    return (
-                        <Dock
-                            key={i}
-                            mc={i+1}
-                            isExpanded={dock}
-                        />
-                    )
-                })
-            }
+        <div>
+            <div className="meter-input">
+                <label htmlFor="meterThreshold">
+                    Meter Threshold
+                    <input
+                        id="meterThreshold"
+                        value={meterThreshold}
+                        placeholder="Meter Threshold"
+                        onChange={(e) => (setMeterThreshold(e.target.value))}
+                    />
+                </label>
+
+            </div>
+
+            <div className="dock-display">
+                {
+                    meterArray.map((dock, i) => {
+                        return (
+                            <Dock
+                                key={i}
+                                mc={i + 1}
+                                isExpanded={dock}
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 };
